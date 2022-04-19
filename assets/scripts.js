f332c8bafba30d617721453a1518180315e36fcd
billyfrn <71398041+billyfrn@users.noreply.github.com>
2022-04-19 14:21:32 -0700

    adding modal

--- assets/scripts.js
+++ assets/scripts.js
@@ -6,3 +6,8 @@ $(document).ready(function(){
     $('#bg-vid').hide();
   },1000);
 })
+
+$('.tile').on('click',function(){
+  whatTile = $(this).data(id)
+  $('.modal[data-id='+whatTile+']').addClass('active');
+})
